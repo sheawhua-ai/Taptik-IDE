1568b2edac75fc3e3b31c8e1c3ae653a346d358a
sheawhua-ai <sheawhua@gmail.com>
2026-06-30 11:47:37 +0800

    chore: remove ESLint configuration
    Clean up project dependencies and config files to remove ESLint as it is no longer required for the codebase.

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
-  env: { browser: true, es2021: true },
-  rules: { "react/react-in-jsx-scope": "off", "no-undef": "off" }
-};
